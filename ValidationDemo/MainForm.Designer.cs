b5f2f16e7785fde36717922451eac60c085d7247
AhmadIzaz <ahmadizaz1996@gmail.com>
2016-12-10 04:05:31 +0500

    Demo of Validation of Empty Fields In C#
    Demonstration of Empty Fields should not be empty,
    Put restriction on Fields to not be empty etc

---
+++ ValidationDemo/MainForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace ValidationDemo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MaingroupBox = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonExir = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.MaingroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MaingroupBox
+            // 
+            this.MaingroupBox.Controls.Add(this.dateTimePicker1);
+            this.MaingroupBox.Controls.Add(this.label4);
+            this.MaingroupBox.Controls.Add(this.comboBox1);
+            this.MaingroupBox.Controls.Add(this.label3);
+            this.MaingroupBox.Controls.Add(this.textBoxAge);
+            this.MaingroupBox.Controls.Add(this.label2);
+            this.MaingroupBox.Controls.Add(this.textBoxName);
+            this.MaingroupBox.Controls.Add(this.label1);
+            this.MaingroupBox.Location = new System.Drawing.Point(18, 12);
+            this.MaingroupBox.Name = "MaingroupBox";
+            this.MaingroupBox.Size = new System.Drawing.Size(485, 187);
+            this.MaingroupBox.TabIndex = 0;
+            this.MaingroupBox.TabStop = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(111, 144);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(173, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Date Of Birth";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "BSC",
+            "MSC",
+            "PHD",
+            "MPHIL",
+            "FA",
+            "FSC"});
+            this.comboBox1.Location = new System.Drawing.Point(111, 96);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(173, 21);
+            this.comboBox1.TabIndex = 2;
+          
+            this.comboBox1.TextChanged += new System.EventHandler(this.Ctrl_Change);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Qualification";
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(111, 53);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(173, 20);
+            this.textBoxAge.TabIndex = 1;
+            this.textBoxAge.TextChanged += new System.EventHandler(this.Ctrl_Change);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Age";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(111, 13);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(173, 20);
+            this.textBoxName.TabIndex = 0;
+            this.textBoxName.TextChanged += new System.EventHandler(this.Ctrl_Change);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // buttonExir
+            // 
+            this.buttonExir.Location = new System.Drawing.Point(275, 222);
+            this.buttonExir.Name = "buttonExir";
+            this.buttonExir.Size = new System.Drawing.Size(100, 56);
+            this.buttonExir.TabIndex = 5;
+            this.buttonExir.Text = "&Exit";
+            this.buttonExir.UseVisualStyleBackColor = true;
+            this.buttonExir.Click += new System.EventHandler(this.buttonExir_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(146, 222);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(100, 56);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "&Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 303);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonExir);
+            this.Controls.Add(this.MaingroupBox);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Validation";
+            this.MaingroupBox.ResumeLayout(false);
+            this.MaingroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox MaingroupBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonExir;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+    }
+}
+
---
+++ ValidationDemo/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ValidationDemo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+
+            if (isvalidated())
+            {
+                MessageBox.Show("New Record Added Succesfully", "Added Succesfully", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private bool isvalidated()
+        {
+            if (textBoxName.Text == string.Empty)
+            {
+                validate(textBoxName, "Name is Required");
+                return false;
+            }
+            if (textBoxAge.Text == string.Empty)
+            {
+                validate(textBoxAge, "Age is required");
+                return false;
+            }
+            else
+            {
+                int outage;
+                if (!int.TryParse(textBoxAge.Text, out outage))
+                {
+                    validate(textBoxAge, "Age Should be integers");
+                    return false;
+                }
+
+            }
+            if (comboBox1.Text == string.Empty)
+            {
+                validate(comboBox1, "Qualification is Required");
+                return false;
+            }
+            if (DateTime.Today.AddYears(-18) > dateTimePicker1.Value.Date)
+            {
+           
+                validate(dateTimePicker1, "You should be over 18");
+                return false;
+            }
+            return true;
+        }
+        private void validate(Control ctrl, string validationmessage)
+        {
+          
+                errorProvider.SetError(ctrl, validationmessage);
+                ctrl.BackColor = Color.LightPink;
+                ctrl.Focus();
+                MessageBox.Show(validationmessage, "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Ctrl_Change(object sender, EventArgs e)
+        {
+            errorProvider.Clear();
+            Control ctr = (Control)sender;
+            ctr.BackColor = Color.White;
+        }
+    }
+}
